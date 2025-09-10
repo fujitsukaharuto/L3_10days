5c008c7d5c6719da45b19c5fbb3b8d8b05dee5d8
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-09-10 21:55:41 +0900

    置けない際の条件式が正しくない不具合を修正しました。 連続して完了ができてしまう不具合を修正しました。

--- project/Game/GameObj/Map/MapField.cpp
+++ project/Game/GameObj/Map/MapField.cpp
@@ -457,8 +457,8 @@ void MapField::UpdateSelectPanel() {
 		r32 halfH = size.y * 0.5f;
 		if (mouse.x >= pos.x - halfW && mouse.x <= pos.x + halfW &&
 			mouse.y >= pos.y - halfH && mouse.y <= pos.y + halfH) {
-			if (Input::GetInstance()->IsTriggerMouse(0) && !haveControlMino_) {
-				if (!controlMino_ || canComplete) {
+			if (Input::GetInstance()->IsTriggerMouse(0) && !haveControlMino_ && arrangement.timer >= 1.0f) {
+				if (!controlMino_ && canComplete) {
 					CompleteArrangement();
 					AudioPlayer::GetInstance()->SoundPlayWave(*push);
 					AudioPlayer::GetInstance()->SoundPlayWave(*machine, 0.15f);
